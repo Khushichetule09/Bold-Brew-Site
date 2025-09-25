26ce42be295719806335aacd0eab3e678146f08a
khushichetule09 <khushichetule2@gmail.com>
2025-09-25 18:33:44 +0530

    update component section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,8 +39,7 @@ const Footer = () => {
                     <div className='py-8 px-4'>
                         <a href="#" className='font-semibold tracking-widest text-2xl sm:text-3xl font-cursive'>Brew Haven</a>
                         <p className='pt-4'>Crafted Coffee, Cozy Vibes, Unforgettable Moments – Your Perfect Espresso Escape</p>
-                        <a href="https://www.youtube.com/@WebeliteBuilders" target='_blank' className='inline-block bg-[#3d2517] py-2 px-4 mt-5 text-sm rounded-full'>Visit our YouTube Channel</a>
-                    </div>
+                        </div>
                     {/* footer links */}
                     <div className='col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10'>
                         <div className='py-8 px-4'>
@@ -70,8 +69,8 @@ const Footer = () => {
                         <div className='py-8 px-4 col-span-2 sm:col-auto'>
                             <h1 className='text-xl font-semibold sm:text-left mb-3'>Address</h1>
                             <div>
-                                <p className='mb-3'>Kolkata, West Bengal</p>
-                                <p>+91 12345467890</p>
+                                <p className='mb-3'>Nagpur, Maharastra</p>
+                                <p>+91 12364498</p>
                                 {/* social links */}
                                 <div className='flex items-center gap-3 mt-6'>
                                     <a href="#">
